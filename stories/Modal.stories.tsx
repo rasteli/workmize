1deb792f16eef80a7cfed80f7c7acdf4f5af352d
rasteli <gabrielrasteli3@gmail.com>
2021-12-01 14:14:34 -0300

    :rocket:

--- stories/Modal.stories.tsx
+++ stories/Modal.stories.tsx
@@ -1,12 +1,21 @@
 import { Story, Meta } from "@storybook/react"
+import { ApolloProvider } from "@apollo/client"
 import { Modal, ModalProps } from "../src/components/Modal"
 import { UserForm } from "../src/components/UserForm"
 
 import User from "../src/assets/user.svg"
+import { client } from "../src/services/api"
 
 export default {
   component: Modal,
-  title: "Components/Modal"
+  title: "Components/Modal",
+  decorators: [
+    Story => (
+      <ApolloProvider client={client}>
+        <Story />
+      </ApolloProvider>
+    )
+  ]
 } as Meta
 
 const Template: Story<ModalProps> = args => <Modal {...args} />
@@ -19,5 +28,5 @@ CreateUser.args = {
       <h1 style={{ marginLeft: 10 }}>Criar usuário</h1>
     </>
   ),
-  children: <UserForm type="signup" />
+  children: <UserForm type="signup" onSubmit={null} />
 }
--- stories/Table.stories.tsx
+++ stories/Table.stories.tsx
@@ -1,11 +1,24 @@
+import { ApolloProvider } from "@apollo/client"
 import { Story, Meta } from "@storybook/react"
 import { Table } from "../src/components/Table"
 
+import { client } from "../src/services/api"
+import { TaskProvider } from "../src/contexts/TaskContext"
+
 export default {
   component: Table,
-  title: "Components/Table"
+  title: "Components/Table",
+  decorators: [
+    Story => (
+      <ApolloProvider client={client}>
+        <TaskProvider>
+          <Story />
+        </TaskProvider>
+      </ApolloProvider>
+    )
+  ]
 } as Meta
 
-// const Template: Story = args => <Table {...args} />
+const Template: Story = args => <Table {...args} />
 
-export const Default = () => <Table />
+export const Default = Template.bind({})
